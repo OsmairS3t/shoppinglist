cc0f04941cb7848933359685ea425843f62d5b23
osmair <osmair144@gmail.com>
2023-10-31 15:28:29 -0300

    updated layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,9 +15,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div>
-          Shopping List
+
+        <div className={styles.header}>
+          <div className={styles.textHeader}>Empresa</div>
+          <div className={styles.textHeader}>Lista</div>
         </div>
+
+        <div className={styles.page}>
+          <div className={styles.menu}>
+            <Link className={styles.itemMenu} href="/">Cadastrar</Link>
+            <Link className={styles.itemMenu} href="/">Listar</Link>
+            <Link className={styles.itemMenu} href="/">Imprimir</Link>
+            <Link className={styles.itemMenu} href="/">Comprar</Link>
+          </div>
+
+          <div className={styles.content}>
+            <div className={styles.blockContent}>
+              <div className={styles.titleBlockContent}>Baixa Urgência</div>
+
+            </div>
+            <div className={styles.blockContent}>
+              <div className={styles.titleBlockContent}>Média Urgência</div>
+            </div>
+            <div className={styles.blockContent}>
+              <div className={styles.titleBlockContent}>Alta Urgência</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.footer}>CopyRight :: 2023</div>
       </main>
     </>
   )
